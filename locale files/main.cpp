ff2169a3a753c30fa6cce52f097154921df3eb2b
AlexSerb <visualstud.c0rp@mail.ru>
2023-10-01 21:26:10 +0700

    .

--- locale files/main.cpp
+++ locale files/main.cpp
@@ -14,7 +14,10 @@ int main()
       case 28:
          printf_s(m == 2 ? "day: 1, month: 3" : "day: 29, month: %d", d);
       case 30:
-         printf_s(m == 4 || m == 6 || m == 9 || m == 11 ? "day: 1, month: %d" : "day: %d, month: %", m, d + 1);
+         printf_s(m == 4 || m == 6 || m == 9 || m == 11 ? "day: 1, month: %d" : "day: %d, month: %d", m, d + 1, m);
+      default:
+         printf_s("");
+
    }
 
    return 0 * _getch();
