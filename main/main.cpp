af2d229f375c930e574fc19a397c1c899ebfd6ba
AlexSerb <visualstud.c0rp@mail.ru>
2023-10-01 22:13:42 +0700

    обновил код

--- main/main.cpp
+++ main/main.cpp
@@ -7,17 +7,21 @@ int main()
    short m = 0, d = 0; //month, day
    setlocale(0, "");
 
-   printf_s("Ââåäèòå m, d: ");
+   printf_s("Введите m, d: ");
    scanf_s("%d %d", &m, &d);
 
    switch (d) {
    case 28:
       printf_s(m == 2 ? "day: 1, month: 3" : "day: 29, month: %d", d);
+      break;
    case 30:
       printf_s(m == 4 || m == 6 || m == 9 || m == 11 ? "day: 1, month: %d" : "day: %d, month: %d", m, d + 1, m);
+      break;
+   case 31:
+      printf_s(m != 12 ? "day: 1, month: %d" : "day: 1, month: 1", m + 1);
+      break;
    default:
-      printf_s("");
-
+      printf_s("day: %d, month: %d", d + 1, m);
    }
 
    return 0 * _getch();
