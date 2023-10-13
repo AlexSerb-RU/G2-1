9f013d6b7775f86f68066cc3eeca7dfad0e04910
AlexSerb <visualstud.c0rp@mail.ru>
2023-10-13 17:48:40 +0700

    лютый тернарник

--- main/main.cpp
+++ main/main.cpp
@@ -10,20 +10,20 @@ int main()
    scanf_s("%hd %hd", &D, &M);
    switch (D)
    {
-   case 28:
-      printf_s(M == 2 ? "День: 1, месяц: 3" : "День: 29, месяц: %d", D);
-      break;
-   case 30:
-      if (M == 4 || M == 6 || M == 9 || M == 11)
-         M++, D = 1;
-      else
-         D++;
-      printf_s("День: %d, месяц: %d", D, M);
-      break;
-   case 31:
-      printf_s(M != 12 ? "День: 1, месяц: %d" : "День: 1, месяц: 1", M + 1);
-      break;
-   default: printf_s("День: %d, месяц: %d", D + 1, M);
+      case 28:
+         printf_s(M == 2 ? "День: 1, месяц: 3" : "День: 29, месяц: %d", D);
+         break;
+      case 30:
+         //if (M == 4 || M == 6 || M == 9 || M == 11)
+         //   M++, D = 1;
+         //else
+         //   D++;
+         printf_s((M == 4 || M == 6 || M == 9 || M == 11 ? "День: 1, месяц: %d" : "День: %d, месяц: %d"), D, M++, D++, M);
+         break;
+      case 31:
+         printf_s(M != 12 ? "День: 1, месяц: %d" : "День: 1, месяц: 1", M + 1);
+         break;
+      default: printf_s("День: %d, месяц: %d", D + 1, M);
    }
    return 0 * _getch();
 }
