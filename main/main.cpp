7077b7cd9dbf744a0e8f8ec08380b6427207c9a2
AlexSerb <visualstud.c0rp@mail.ru>
2023-10-13 18:38:57 +0700

    0%

--- main/main.cpp
+++ main/main.cpp
@@ -4,26 +4,17 @@
 
 int main()
 {
-   short D = 0, M = 0; //день, месяц
+   char D = 0, M = 0;  // день, месяц
    setlocale(0, "");
    printf_s("Введите D, M: ");
    scanf_s("%hd %hd", &D, &M);
    switch (D)
    {
-      case 28:
-         printf_s(M == 2 ? "День: 1, месяц: 3" : "День: 29, месяц: %d", D);
-         break;
-      case 30:
-         //if (M == 4 || M == 6 || M == 9 || M == 11)
-         //   M++, D = 1;
-         //else
-         //   D++;
-         printf_s((M == 4 || M == 6 || M == 9 || M == 11 ? "День: 1, месяц: %d" : "День: %d, месяц: %d"), D, M++, D++, M);
-         break;
-      case 31:
-         printf_s(M != 12 ? "День: 1, месяц: %d" : "День: 1, месяц: 1", M + 1);
-         break;
-      default: printf_s("День: %d, месяц: %d", D + 1, M);
+      case 28: (M == 2 ? D = 1, M : D)++;
+      case 30: (M == 4 || M == 6 || M == 9 || M == 11 ? D = 1, M : D)++;
+      case 31: (M == 12 ? M = D : M++, D) = 1;
+      default: (D == 29 || D < 28 ? D++ : M = M);
    }
+   printf_s("day: %d, month: %d", D, M);
    return 0 * _getch();
 }
