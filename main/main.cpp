89ac17d3b8742cfd271562eb4e35fe77d18426c5
AlexSerb <visualstud.c0rp@mail.ru>
2023-10-14 11:31:46 +0700

    +sln - условие default + русификация

--- main/main.cpp
+++ main/main.cpp
@@ -7,14 +7,14 @@ int main()
    char D = 0, M = 0;  // день, месяц
    setlocale(0, "");
    printf_s("Введите D, M: ");
-   scanf_s("%hd %hd", &D, &M);
+   scanf_s("%hhd %hhd", &D, &M);
    switch (D)
    {
       case 28: (M == 2 ? D = 1, M : D)++; break;
       case 31: (M == 12 ? M = D : M++, D) = 1; break;
       case 30: (M == 4 || M == 6 || M == 9 || M == 11 ? D = 1, M : D)++; break;
-      default: (D == 29 || D < 28 ? D++ : M = M);
+      default: D++;
    }
-   printf_s("day: %d, month: %d", D, M);
+   printf_s("День: %d, Месяц: %d", D, M);
    return 0 * _getch();
 }
